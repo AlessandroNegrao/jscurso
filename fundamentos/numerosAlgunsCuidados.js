807fc76b223fa5fdb8dce653a691332c1f8cad7a
Alessandro Negrão <alessandro.negrao.s@gmail.com>
2019-09-21 11:11:33 -0400

    add code about the warnings about uses of numbers on Javascript

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,23 @@
+// retorna o tipo infinity
+console.log(7/0)
+
+// o js permite dividir strings se só tiver numeros na mesma
+console.log("10"/5)
+
+// O js não permite que se faça esta operação
+console.log("Show!" * 2);
+
+// algumas operações com pontos flutuantes podem não render resultados precisos
+// isso porque é parte de uma especificação da IEEE 
+
+console.log(0.1 + 0.7)
+
+// Chamar um numero literal nao permite que se execute funções
+//console.log(10.toString)
+// Mas se pode se colocar o numero dentro de parenteses, ou usando uma variavel
+// e o atribuindo a ela
+
+console.log((10).toString)
+
+var a = 10
+console.log(a.toString)
